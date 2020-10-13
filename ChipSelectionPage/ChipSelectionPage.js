381a54abde12d39ff06e4ad6682928b22c29e139
Muhammad Muizz Zafar <muizzzafar25351964@gmail.com>
2020-10-13 19:35:43 -0400

    fixed error 2

--- ChipSelectionPage/ChipSelectionPage.js
+++ ChipSelectionPage/ChipSelectionPage.js
@@ -1,9 +1,7 @@
 function checkSignIn() {
-    var userIsLoggedIn = `<button onclick="changePage(3)">click me</button>`;
-    var userIsNotLoggedIn = `<button onclick="changePage(5)">click me</button>`;
     if (window.localStorage.getItem("Email").length > 0) {
-        document.getElementById("chipSelection").innerHTML = changePage(3, 'subjectToChange');
+        changePage(3, 'subjectToChange');
     } else {
-        document.getElementById("chipSelection").innerHTML = changePage(5, 'subjectToChange');
+        changePage(5, 'subjectToChange');
     }
 }
