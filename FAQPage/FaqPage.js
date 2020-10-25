7f324656d14afdced4e809266e5d711cb267aeca
Robert Djukic <rdjuk093@uottawa.ca>
2020-10-25 19:25:23 -0400

    FAQ Page

---
+++ FAQPage/FaqPage.js
@@ -0,0 +1,8 @@
+var myFunction = function() {
+    var dropdown1t = document.getElementById("drowdown1");
+    dropdown1.textContent = "Answer";
+};
+    
+ var button1 = document.getElementById("button1");
+ button1.addEventListener("click", myFunction);
+       
