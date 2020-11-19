1e83a44d3e154f8693cb306d1fece510c979af62
Robert Djukic <rdjuk093@uottawa.ca>
2020-11-19 11:58:25 -0500

    Adding changes to the Faq Page

--- FAQPage/FaqPage.js
+++ FAQPage/FaqPage.js
@@ -1,8 +1,35 @@
-var myFunction = function() {
-    var dropdown1t = document.getElementById("drowdown1");
-    dropdown1.textContent = "Answer";
-};
-    
- var button1 = document.getElementById("button1");
- button1.addEventListener("click", myFunction);
-       
+document.getElementById("prod").onclick = function() { 
+  
+    document.getElementById("product").style.display = "block";
+    document.getElementById("general").style.display = "none"; 
+    document.getElementById("tech").style.display = "none";  
+    document.getElementById("Qsection").innerText = "Product Specification";
+
+} 
+
+document.getElementById("gen").onclick = function() { 
+
+    document.getElementById("general").style.display = "block"; 
+    document.getElementById("product").style.display = "none"; 
+    document.getElementById("tech").style.display = "none"; 
+    document.getElementById("Qsection").innerText = "General Questions";
+
+} 
+
+document.getElementById("tec").onclick = function() { 
+
+    document.getElementById("tech").style.display = "block"; 
+    document.getElementById("product").style.display = "none"; 
+    document.getElementById("general").style.display = "none"; 
+    document.getElementById("Qsection").innerText = "Tech Questions";
+
+} 
+
+document.getElementById("all").onclick = function() { 
+  
+    document.getElementById("general").style.display = "block"; 
+    document.getElementById("tech").style.display = "block"; 
+    document.getElementById("product").style.display = "block"; 
+    document.getElementById("Qsection").innerText = "All Questions";
+
+} 
