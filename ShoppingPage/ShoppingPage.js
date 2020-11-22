8abcf4ed3f34bfe3fd5dee1f544d645f1ca112e5
Muhammad Muizz Zafar <muizzzafar25351964@gmail.com>
2020-11-22 04:06:00 -0500

    Commit for TAHA BEN AMOR

--- ShoppingPage/ShoppingPage.js
+++ ShoppingPage/ShoppingPage.js
@@ -1,3 +1,12 @@
 document.getElementById("toShoeSelectPage-btn").onclick = function() {
     location.href = "../ShoeSelectionPage/ShoeSelectionPage.html";
 };
+
+
+
+document.getElementById("toLoginPage-btn").onclick = function() {
+
+    if (window.localStorage.getItem)
+
+        location.href = "../LoginPage/Loginpage.html";
+};
