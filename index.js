606800a9dbb585f7736d0c3d1f38e863902c8c59
Muhammad Muizz Zafar <muizzzafar25351964@gmail.com>
2020-11-22 04:13:06 -0500

    resolving merge conflicts

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+var shoImageslInk = [
+    "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1558004282-e2b2587e3e47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1543508282-5c1f427f023f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+]
+
+
 var pageLinks = [
     "LoginPage/LoginPage.html",
-    "ShoeSelectionPage/ShoeSelectionPage.html",
-    "chipSelectionPage/chipSelectionPage.html"
 ];
 
 function changePage(index, divToChange) {
